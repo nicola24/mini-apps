3284610b9a4d199b4a514c3bf6a3819469f6383d
Nicolas Peyrichou <nicolas.peyrichou@live.fr>
2018-02-12 11:49:38 -0800

    add title, table and X on click

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,50 @@
+// When a square is clicked, add an X to that square.
+var r1c1 = document.getElementById("r1c1");
+r1c1.addEventListener("click", () => {
+  r1c1.innerHTML = "X";
+}, false);
 
+var r1c1 = document.getElementById("r1c1");
+r1c1.addEventListener("click", () => {
+  r1c1.innerHTML = "O";
+}, false);
 
+var r1c2 = document.getElementById("r1c2");
+r1c2.addEventListener("click", () => {
+  r1c2.innerHTML = "X";
+}, false);
+
+var r1c3 = document.getElementById("r1c3");
+r1c3.addEventListener("click", () => {
+  r1c3.innerHTML = "X";
+}, false);
+
+var r2c1 = document.getElementById("r2c1");
+r2c1.addEventListener("click", () => {
+  r2c1.innerHTML = "X";
+}, false);
+
+var r2c2 = document.getElementById("r2c2");
+r2c2.addEventListener("click", () => {
+  r2c2.innerHTML = "X";
+}, false);
+
+var r2c3 = document.getElementById("r2c3");
+r2c3.addEventListener("click", () => {
+  r2c3.innerHTML = "X";
+}, false);
+
+var r3c1 = document.getElementById("r3c1");
+r3c1.addEventListener("click", () => {
+  r3c1.innerHTML = "X";
+}, false);
+
+var r3c2 = document.getElementById("r3c2");
+r3c2.addEventListener("click", () => {
+  r3c2.innerHTML = "X";
+}, false);
+
+var r3c3 = document.getElementById("r3c3");
+r3c3.addEventListener("click", () => {
+  r3c3.innerHTML = "X";
+}, false);
