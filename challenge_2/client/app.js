ef1849d18e4a3a52aba16dced6c7b370127274bd
nicola24 <nicolas.peyrichou@gmail.com>
2018-02-13 14:01:13 -0800

    working on app.js

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,31 +1,31 @@
-// let $firstName = $('#firstname');
-// let $lastName = $('#lastname');
-// let $county = $('#county');
-// let $city = $('#city');
-// let $role = $('#role');
-// let $sales = $('#sales');
-
 //function that handle submit button
 $( "form" ).submit(function( event ) {
-  console.log('submit');
-  event.preventDefault();
+  //object that store data from form and submit it
+  let data = {
+      firstName: $('#firstname').val(),
+      lastName: $('#lastname').val(),
+      county: $('#county').val(),
+      city: $('#city').val(),
+      role: $('#role').val(),
+      sales: $('#sales').val(),
+    };
+    // print data in the div result
+    $('#result').text(JSON.stringify(data));
+    // prevent submit from reloading page
+    event.preventDefault();
 });
 
+// ****************************************************************************
 
-let get = $.ajax({
-  type: 'GET';
-  URL: 'localhost:3000';
-  success: function(data) {
-    console.log(data);
-  };
-  error: function(); 
-});
-
-let post = $.ajax({
-  type: 'POST';
-  URL: 'localhost:3000';
-  success: function(result) {
-    console.log(result);
-  };
-  error: function();
-});
+//ajax request
+// const ajaxRequest = {
+//   $.ajax({
+//     method: 'POST',
+//     data: JSON.stringify(data),
+//     URL: 'localhost:3000',
+//     success: function(data) {
+//       console.log(data);
+//     };
+//     error: function();
+//   });
+// };
