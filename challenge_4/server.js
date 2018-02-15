ed7825d5858ee73dc3618b3e9bcfed8d50f92c36
nicola24 <nicolas.peyrichou@gmail.com>
2018-02-15 11:08:53 -0800

    server up and running + react, babel, webpack set up

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,7 @@
+const express = require('express');
+const app = express();
+app.use(express.static('client'));
 
+app.get('/', (req, res) => res.send('Hello World!'));
 
+app.listen(3000, () => console.log('App listening on port 3000!'));
