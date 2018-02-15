6862508b7ce9a0aa7c4dc58c5f828fc06d0e1a06
nicola24 <nicolas.peyrichou@gmail.com>
2018-02-14 16:24:45 -0800

    displaying column

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,28 +1,88 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-class App extends React.Component {
-  render () {
-    return <h2>app.jsx</h2>;
+//*** COMPONENT BOARD ***
+class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Connect Four React</h1>
+        </div>
+        <div>
+          <div class="column">
+            <Square x={0} y={5}/>
+            <Square x={0} y={4}/>
+            <Square x={0} y={3}/>
+            <Square x={0} y={2}/>
+            <Square x={0} y={1}/>
+            <Square x={0} y={0}/>
+          </div>
+          <div class="column">
+            <Square x={1} y={5}/>
+            <Square x={1} y={4}/>
+            <Square x={1} y={3}/>
+            <Square x={1} y={2}/>
+            <Square x={1} y={1}/>
+            <Square x={1} y={0}/>
+          </div>
+          <div class="column">
+            <Square x={2} y={5}/>
+            <Square x={2} y={4}/>
+            <Square x={2} y={3}/>
+            <Square x={2} y={2}/>
+            <Square x={2} y={1}/>
+            <Square x={2} y={0}/>
+          </div>
+          <div class="column">
+            <Square x={3} y={5}/>
+            <Square x={3} y={4}/>
+            <Square x={3} y={3}/>
+            <Square x={3} y={2}/>
+            <Square x={3} y={1}/>
+            <Square x={3} y={0}/>
+          </div>
+          <div class="column">
+            <Square x={4} y={5}/>
+            <Square x={4} y={4}/>
+            <Square x={4} y={3}/>
+            <Square x={4} y={2}/>
+            <Square x={4} y={1}/>
+            <Square x={4} y={0}/>
+          </div>
+          <div class="column">
+            <Square x={5} y={5}/>
+            <Square x={5} y={4}/>
+            <Square x={5} y={3}/>
+            <Square x={5} y={2}/>
+            <Square x={5} y={1}/>
+            <Square x={5} y={0}/>
+          </div>
+          <div class="column">
+            <Square x={6} y={5}/>
+            <Square x={6} y={4}/>
+            <Square x={6} y={3}/>
+            <Square x={6} y={2}/>
+            <Square x={6} y={1}/>
+            <Square x={6} y={0}/>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-render(<App/>, document.getElementById('app'));
-
-//*** COMPONENT BOARD ***
-// class Board extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//           <h1>Connect Four React</h1>
-//       </div>
-//     );
-//   }
-// }
-
 // *** COMPONENT SQUARE ***
-// const Square = (props) => (
-//   <div>
-//       {this.props.x}{this.props.y}
-//   </div>
-// );
+const Square = (props) => (
+  <div>
+    <div id="oneColumn">{props.x}{props.y}</div>
+  </div>
+);
+
+render(<Board />, document.getElementById('app'));
